9e251a1398ded26e35895016932ea0e88994f62b
Frank Chao <aaefiikmnnnr@gmail.com>
2017-08-26 17:33:26 +0800

    Add AssetDatabase find example

---
+++ Assets/02AssetDatabase/Scripts/Editor/AssetDatabaseDemo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class AssetDatabaseDemo
+{
+    [MenuItem("ToolsDemo/List Assets")]
+    public static void ListAssets()
+    {
+        string[] textureGUIDs = AssetDatabase.FindAssets("t:Texture",
+                                                         new string[] {"Assets/02AssetDatabase"});
+        foreach (string guid in textureGUIDs)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            Debug.Log(path);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Icons used for the demo is from Google Material Components:
+https://github.com/material-components
