ca3512e1231cbf4bc9e1099798a160d29bc32931
Frank Chao <aaefiikmnnnr@gmail.com>
2017-08-26 16:44:48 +0800

    Add comment about activeGameObject

--- Assets/Scripts/01MenuItem/MenuItem.cs
+++ Assets/Scripts/01MenuItem/MenuItem.cs
@@ -6,6 +6,7 @@ public class MenuItemDemo
     [MenuItem("ToolsDemo/Rename")]
     public static void Rename()
     {
+		// activeGameObject == The one appear on Inspector
         if (Selection.activeGameObject == null)
         {
             Debug.Log("No active GameObject selected");
