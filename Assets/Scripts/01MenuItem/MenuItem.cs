0cc701c25154e3d4e8c681a9f3859170a2000363
Chao Chun-Fu <chao3@rayark.com>
2017-08-26 16:12:53 +0800

    Commit MenuItem and Selection example

---
+++ Assets/Scripts/01MenuItem/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MenuItemDemo
+{
+    [MenuItem("ToolsDemo/Rename")]
+    public static void Rename()
+    {
+        if (Selection.activeGameObject == null)
+        {
+            Debug.Log("No active GameObject selected");
+            return;
+        }
+
+        Selection.activeGameObject.name = "Renamed";
+    }
+}
