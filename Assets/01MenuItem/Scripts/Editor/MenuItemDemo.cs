f14d1b2a52ae11f9da8ea08279db634c54c152fe
Frank Chao <aaefiikmnnnr@gmail.com>
2017-08-27 15:41:12 +0800

    Add dirty flag examples

--- Assets/01MenuItem/Scripts/Editor/MenuItemDemo.cs
+++ Assets/01MenuItem/Scripts/Editor/MenuItemDemo.cs
@@ -1,18 +1,67 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEditor;
+using UnityEditor.SceneManagement;
+
 
 public class MenuItemDemo
 {
-    [MenuItem("ToolsDemo/Rename")]
+    [MenuItem("ToolsDemo/MenuItem/Rename", isValidateFunction: false, priority: 0)]
     public static void Rename()
     {
-		// activeGameObject == The one appear on Inspector
-        if (Selection.activeGameObject == null)
+        // activeGameObject == The one appear on Inspector
+        var selectedGameObject = Selection.activeGameObject;
+        if (selectedGameObject == null)
         {
             Debug.Log("No active GameObject selected");
             return;
         }
 
-        Selection.activeGameObject.name = "Renamed";
+        selectedGameObject.name = "Renamed";
+    }
+
+
+    [MenuItem("ToolsDemo/MenuItem/Rename with scene dirty flag", isValidateFunction: false, priority: 1)]
+    public static void RenameWithSceneDirtyFlag()
+    {
+        var selectedGameObject = Selection.activeGameObject;
+        if (selectedGameObject == null)
+        {
+            Debug.Log("No active GameObject selected");
+            return;
+        }
+
+        selectedGameObject.name = "Renamed";
+        EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
+    }
+
+
+    [MenuItem("ToolsDemo/MenuItem/Set layer with dirty flag", isValidateFunction: false, priority: 1)]
+    public static void RenameWithDirtyFlag()
+    {
+        var selectedGameObject = Selection.activeGameObject;
+        if (selectedGameObject == null)
+        {
+            Debug.Log("No active GameObject selected");
+            return;
+        }
+
+        selectedGameObject.layer = 2;
+        EditorUtility.SetDirty(selectedGameObject);
+    }
+
+
+    [MenuItem("ToolsDemo/MenuItem/Rename with Undo", isValidateFunction: false, priority: 2)]
+    public static void RenameWithUndo()
+    {
+        var selectedGameObject = Selection.activeGameObject;
+        if (selectedGameObject == null)
+        {
+            Debug.Log("No active GameObject selected");
+            return;
+        }
+
+        Undo.RecordObject(Selection.activeGameObject, "My Rename GameObject");
+        selectedGameObject.name = "Renamed";
     }
 }
