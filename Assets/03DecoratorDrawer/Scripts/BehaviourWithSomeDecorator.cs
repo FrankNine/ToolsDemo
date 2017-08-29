11accf2f4c94a451e660b60acfe2e748e1f470e1
Frank Chao <aaefiikmnnnr@gmail.com>
2017-08-29 09:15:20 +0800

    Add inspector decorator

---
+++ Assets/03DecoratorDrawer/Scripts/BehaviourWithSomeDecorator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourWithSomeDecorator : MonoBehaviour
+{
+	[Header("I am title")]
+
+	[SerializeField]
+	private string myName;
+
+	[Space(20)]
+	[SerializeField]
+	private int iAmAnIntNotRelatedToMyName;
+}
